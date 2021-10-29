f29aab199b9db890b5270ef6841b0582cb53a65a
Carlos Alberto <carlosjunior.alberto4@gmail.com>
2021-10-28 22:43:52 -0300

    teste

--- frontend/utils/axios.ts
+++
@@ -1,9 +0,0 @@
-import Axios from 'axios';
-
-const instance = Axios.create({
-    baseURL:"http://localhost:1337/parse"
-});
-
-instance.defaults.headers.common['X-Parse-Application-Id'] = 'flight'
-
-export default instance;
